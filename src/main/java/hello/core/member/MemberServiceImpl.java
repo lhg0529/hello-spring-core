39425c5ad3a6c59038f30441f6056dbdd0f11880
lhg0529 <lhg0529@naver.com>
2023-11-08 17:32:21 +0900

    231108

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-  private final MemberRepository memberRepository = new MemoryMemberRepository();
-//memberRepository로 만들어서 만들면 나중에 메모리멤버리포토리 부분을 갈아 끼우기만 하면 됨
+  private final MemberRepository memberRepository; //원래 메모리멤버저장소가 new로 있었는데 AppConfig만들면서 분리
+
+  public MemberServiceImpl(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
+
+  //memberRepository로 만들어서 만들면 나중에 메모리멤버리포토리 부분을 갈아 끼우기만 하면 됨
   @Override
   public void join(Member member) {
     memberRepository.save(member);
---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+  private final MemberRepository memberRepository;
+  private final DiscountPolicy discountPolicy; //이렇게만 두면 DIP, OCP 위반이 아닌데 이대로는 오류.
+
+  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    this.memberRepository = memberRepository;
+    this.discountPolicy = discountPolicy;
+  }
+
+  @Override
+  public Order createOrder(Long memberId, String itemName, int itemPrice) {
+    Member member = memberRepository.findById(memberId);
+    int discountPrice = discountPolicy.discount(member, itemPrice);
+
+    return new Order(memberId,itemName,itemPrice,discountPrice);
+  }
+}
